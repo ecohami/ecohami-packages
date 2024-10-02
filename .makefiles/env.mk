52a66682975d41c0bdd97054393e8f7f4aa10d68
phoax <factorimio@gmail.com>
2024-10-02 22:48:39 +0200

    feat: tools

--- .makefiles/env.mk
+++ .makefiles/env.mk
@@ -1,5 +1,8 @@
+include .env.example
+-include .env
+
 .PHONY: env
-env: ## Prepares the .env file by copying from .env.example if not present and replacing placeholders with JustChangeMe.
+env: ## Prepares the .env file by copying from .env.example.\n If not present and replacing password placeholders with JustChangeMe.
 ifeq ($(OS),Windows_NT)
 	@if not exist .env ( \
 		echo "No .env file found. Creating from .env.example..." && \
---
+++ tools/cli/main.mjs
@@ -0,0 +1,5 @@
+// Internal
+import { CLI } from './src/cli.mjs';
+
+const cli = new CLI();
+cli.main();
